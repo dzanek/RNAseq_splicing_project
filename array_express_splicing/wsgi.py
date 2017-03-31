afab7d0a575b8957776b3691dac2ba06a6dcbb82
jan.majta@ardigen.com <jan.majta@ardigen.com>
2017-03-31 16:53:13 +0200

    django set up

---
+++ array_express_splicing/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for array_express_splicing project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "array_express_splicing.settings")
+
+application = get_wsgi_application()
